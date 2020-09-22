c9ba129cc1b47430a778da67764310e6a2cc5a1f
Rutwik1801 <rutwik18012001@gmail.com>
2020-09-23 02:09:34 +0800

    Updates

---
+++ precache-manifest.d0b9f3b3b5446575b3568152a8abfe93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19c5ad7792e59722ec444d4b5647b328",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "40098712fcd3b05038b6",
+    "url": "/monsters-rolodex/static/css/main.1863271d.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40098712fcd3b05038b6",
+    "url": "/monsters-rolodex/static/js/main.b310b196.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
